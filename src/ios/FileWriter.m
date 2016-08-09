1defe1ad5eac1653466fbb6dc4d17b8ebc63c2d9
RahulS <rahulsak2005@gmail.com>
2016-08-08 22:50:50 -0700

    Update FileWriter.m

--- src/ios/FileWriter.m
+++ src/ios/FileWriter.m
@@ -1,9 +1,7 @@
 //
 //  FileWriter.m
 //  CustomPlugin
-//
 //  Created by Mac Dev Machine on 12/4/15.
-//
 //
 
 #import "FileWriter.h"
@@ -13,39 +11,29 @@
 -(void)cordovaGetFileContents:(CDVInvokedUrlCommand *)command
 {
     NSString *dateStr = [self getFileContents];
-    
     NSDictionary *jsonObj = [[NSDictionary alloc] initWithObjectsAndKeys:
                              dateStr,@"dateStr",
                              @"true", @"success",nil];
     
-    
-    
     CDVPluginResult *pluginResult = [CDVPluginResult
                                      resultWithStatus: CDVCommandStatus_OK
                                     messageAsDictionary:jsonObj];
-    
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void) cordovaSetFileContents:(CDVInvokedUrlCommand *)command {
-
     NSString *dateStr = [command.arguments objectAtIndex:0];
-    
-    
     [self setFileContents: dateStr];
-    
+
     // Create an object with a simple success property.
     NSDictionary *jsonObj = [ [NSDictionary alloc]
                              initWithObjectsAndKeys :
                              @"true", @"success",
-                             nil
-                             ];
+                             nil];
     
     CDVPluginResult *pluginResult = [ CDVPluginResult
                                      resultWithStatus    : CDVCommandStatus_OK
-                                     messageAsDictionary : jsonObj
-                                     ];
-    
+                                     messageAsDictionary : jsonObj];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
@@ -53,14 +41,10 @@
 
 //set the file contents 
 - (void) setFileContents:(NSString *)fileContents {
-    
 
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    
     NSString *documentsDirectory = [paths objectAtIndex:0];
-    
     NSString *fileName = [NSString stringWithFormat:@"%@/myTextFile.txt", documentsDirectory];
-    
     [fileContents writeToFile : fileName
                   atomically  : NO
                   encoding    : NSStringEncodingConversionAllowLossy
@@ -69,20 +53,15 @@
 
 //get the file contents 
 - (NSString *) getFileContents{
-    
 
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    
     NSString *documentsDirectory = [paths objectAtIndex:0];
-    
     NSString *fileName = [NSString stringWithFormat:@"%@/myTextFile.txt", documentsDirectory];
-    
 
     NSString *fileContents = [[NSString alloc]
                               initWithContentsOfFile : fileName
                               usedEncoding           : nil
-                              error                  : nil
-                              ];
+                              error                  : nil];
     return fileContents;
 }
 

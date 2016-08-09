ce0a86cc0116f906b6b5aa64a931a4b10a2e9e78
RahulS <rahulsak2005@gmail.com>
2016-08-08 22:49:12 -0700

    Update FileWriter.h

--- src/ios/FileWriter.h
+++ src/ios/FileWriter.h
@@ -1,27 +1,22 @@
 //
 //  FileWriter.h
 //  CustomPlugin
-//
 //  Created by Mac Dev Machine on 12/4/15.
 //
-//
+
 
 #import <Cordova/CDV.h>
-
 @interface FileWriter : CDVPlugin
 
 // returns the file content in a JSON object 
 - (void) cordovaGetFileContents:(CDVInvokedUrlCommand *)command;
-
 - (void) cordovaSetFileContents:(CDVInvokedUrlCommand *)command;
 
 #pragma mark - Methods
-
 // code to persist data
 - (void) setFileContents:(NSString *)str;
 
 // code to load data from disk and return the String.
 - (NSString *) getFileContents;
-
 
 @end
